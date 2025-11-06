650f670349dc870e32a6b5c06ad2963a2815d1eb
Hareesh <hareesh.madinedi@gmail.com>
2025-11-06 11:36:59 +0530

    Day1

---
+++ Day1_intro_var/var.py
@@ -0,0 +1,22 @@
+# Variable dec
+# gen
+a=10
+print(a)
+# group
+b,c,d=20,30,40
+print(b,c,d)
+# same
+e=f=g=50
+print(e,f,g)
+e+=10
+print(e)
+print(f)
+
+name='Hareesh'
+print(name)
+_name='smith'
+print(_name)
+Name='Peter'
+print(Name)
+NAME='Banner'
+print(NAME,Name,_name,name)
