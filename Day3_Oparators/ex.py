dd4f10057fd4b21a454dcafa676681ea81f789ed
Hareesh <hareesh.madinedi@gmail.com>
2025-11-09 17:37:32 +0530

    Day3_oparators

---
+++ Day3_Oparators/ex.py
@@ -0,0 +1,77 @@
+# oparation: the action performed by an oparator between two oparaends
+
+print(10+20)
+
+# Arthamatic
+'''
++ addition values and string
+- sub
+* mul values and strings
+/ divion
+% mod
+// flor divison
+** expo
+'''
+# assignment
+'''
+= (a=10)
++=   (a+=10)
+-=(a-=10)
+/=
+%=
+//=
+**=
+'''
+# relation or comparision
+'''
+>
+<
+==
+>=
+<=
+!=
+'''
+# logical (bus or car or bike) (buy a ticket and verifying the ticket) ( not false)
+'''
+or 
+and 
+not 
+'''
+# membership oparators
+'''
+in
+not in
+'''
+# identity oparators
+'''
+is 
+is not
+'''
+g=10
+h='hello'
+
+print(g+h)
+
+
+# print("Achiver\'s"+'IT' '')
+
+# # print('Hello'*3) '''strint repetation''' 
+# print('Hello\n'*3) 
+
+# print(10**10)
+
+# a=10
+# a+=10 
+# print(a)
+# a-=10 
+# print(a)
+# a**=2
+
+# print(10%2==0)
+
+# print(10!=2)
+
+# print(not  10//2==0) 
+
+
+# print('A' not in 'Achiever\'s')
